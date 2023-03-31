1f95eeccd9f7cf3f4fdccf773930277306620f2d
Ariel Lubonja <ariel@cs.jhu.edu>
2023-03-31 16:14:21 -0400

    Trains on GPU but Dev acc <= 30%

--- utils/accuracies.py
+++ utils/accuracies.py
@@ -1,9 +1,9 @@
 import torch
-import numpy as np
 import torch.nn.functional as F
+from numpy.random import choice
 
 
-def accuracy(y : np.ndarray, y_hat : np.ndarray) -> np.float64:
+def accuracy(y, y_hat):
     """Calculate the simple accuracy given two numpy vectors, each with int values
     corresponding to each class.
 
@@ -17,10 +17,10 @@ def accuracy(y : np.ndarray, y_hat : np.ndarray) -> np.float64:
     ### TODO Implement accuracy function
     
     
-    return np.sum(y == y_hat) / len(y)
+    return torch.sum(y == y_hat) / len(y)
 
 
-def approx_train_acc_and_loss(model, train_data : np.ndarray, train_labels : np.ndarray) -> np.float64:
+def approx_train_acc_and_loss(model, train_data, train_labels):
     """Given a model, training data and its associated labels, calculate the simple accuracy when the 
     model is applied to the training dataset.
     This function is meant to be run during training to evaluate model training accuracy during training.
@@ -33,7 +33,7 @@ def approx_train_acc_and_loss(model, train_data : np.ndarray, train_labels : np.
     Returns:
         np.float64: simple accuracy
     """
-    idxs = np.random.choice(len(train_data), 100, replace=False)
+    idxs = choice(len(train_data), 100, replace=False)
     # x = torch.from_numpy(train_data[idxs].astype(np.float32))
     x = train_data[idxs]
     # y = torch.from_numpy(train_labels[idxs].astype(np.int))
@@ -41,10 +41,10 @@ def approx_train_acc_and_loss(model, train_data : np.ndarray, train_labels : np.
     logits = model(x)
     loss = F.cross_entropy(logits, y)
     y_pred = torch.max(logits, 1)[1]
-    return accuracy(train_labels[idxs], y_pred.numpy()), loss.item()
+    return accuracy(train_labels[idxs], y_pred), loss.item()
 
 
-def dev_acc_and_loss(model, dev_data : np.ndarray, dev_labels : np.ndarray) -> np.float64:
+def dev_acc_and_loss(model, dev_data, dev_labels):
     """Given a model, a validation dataset and its associated labels, calcualte the simple accuracy when the
     model is applied to the validation dataset.
     This function is meant to be run during training to evaluate model validation accuracy.
