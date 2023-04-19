935c17d56ba1664ed1c690cf8208c424edae5345
Ariel Lubonja <ariel@cs.jhu.edu>
2023-04-18 23:37:50 -0400

    CPU high accuracy + Albumentations

--- utils/accuracies.py
+++ utils/accuracies.py
@@ -1,9 +1,9 @@
 import torch
+import numpy as np
 import torch.nn.functional as F
-from numpy.random import choice
 
 
-def accuracy(y, y_hat):
+def accuracy(y : np.ndarray, y_hat : np.ndarray) -> np.float64:
     """Calculate the simple accuracy given two numpy vectors, each with int values
     corresponding to each class.
 
@@ -17,10 +17,10 @@ def accuracy(y, y_hat):
     ### TODO Implement accuracy function
     
     
-    return torch.sum(y == y_hat) / len(y)
+    return np.sum(y == y_hat) / len(y)
 
 
-def approx_train_acc_and_loss(model, train_data, train_labels):
+def approx_train_acc_and_loss(model, train_data : np.ndarray, train_labels : np.ndarray) -> np.float64:
     """Given a model, training data and its associated labels, calculate the simple accuracy when the 
     model is applied to the training dataset.
     This function is meant to be run during training to evaluate model training accuracy during training.
@@ -33,18 +33,16 @@ def approx_train_acc_and_loss(model, train_data, train_labels):
     Returns:
         np.float64: simple accuracy
     """
-    idxs = choice(len(train_data), 100, replace=False)
-    # x = torch.from_numpy(train_data[idxs].astype(np.float32))
-    x = train_data[idxs]
-    # y = torch.from_numpy(train_labels[idxs].astype(np.int))
-    y = train_labels[idxs]
+    idxs = np.random.choice(len(train_data), 100, replace=False)
+    x = torch.from_numpy(train_data[idxs].astype(np.float32))
+    y = torch.from_numpy(train_labels[idxs].astype(np.int))
     logits = model(x)
     loss = F.cross_entropy(logits, y)
     y_pred = torch.max(logits, 1)[1]
-    return accuracy(train_labels[idxs], y_pred), loss.item()
+    return accuracy(train_labels[idxs], y_pred.numpy()), loss.item()
 
 
-def dev_acc_and_loss(model, dev_data, dev_labels):
+def dev_acc_and_loss(model, dev_data : np.ndarray, dev_labels : np.ndarray) -> np.float64:
     """Given a model, a validation dataset and its associated labels, calcualte the simple accuracy when the
     model is applied to the validation dataset.
     This function is meant to be run during training to evaluate model validation accuracy.
@@ -57,11 +55,9 @@ def dev_acc_and_loss(model, dev_data, dev_labels):
     Returns:
         np.float64: simple validation accuracy
     """
-    # x = torch.from_numpy(dev_data.astype(np.float32))
-    x = dev_data
-    # y = torch.from_numpy(dev_labels.astype(np.int))
-    y = dev_labels
+    x = torch.from_numpy(dev_data.astype(np.float32))
+    y = torch.from_numpy(dev_labels.astype(np.int))
     logits = model(x)
     loss = F.cross_entropy(logits, y)
     y_pred = torch.max(logits, 1)[1]
-    return accuracy(dev_labels, y_pred), loss.item()
+    return accuracy(dev_labels, y_pred.numpy()), loss.item()
